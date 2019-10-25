efa0605a46dafc760b7ca8fcad0c25ac45e2c4f9
audraybinet <audreybinet@yahoo.fr>
2019-10-25 12:05:33 +0200

    exo5

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+function multiplate(){
+var firstNumber= document.getElementById('firstNumber').value;
+firstNumber= Math.trunc(firstNumber);
+var secondNumber= document.getElementById('secondNumber').value;
+alert(firstNumber * secondNumber);
+}
